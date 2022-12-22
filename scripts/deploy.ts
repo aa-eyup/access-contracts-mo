6ace9927ba8ce823b5dbbd804f5fcdac425a2cd8
Alpay Aldemir <alpayaldemir@bitgo.com>
2022-12-22 12:08:43 -0500

    install packages

---
+++ scripts/deploy.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const contractName = '';
+  const Contract = await ethers.getContractFactory(contractName);
+  // pass in constructor arguments
+  const contract = await Contract.deploy();
+
+  await contract.deployed();
+
+  console.log(`deployed to ${contract.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
