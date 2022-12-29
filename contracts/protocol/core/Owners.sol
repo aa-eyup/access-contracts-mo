d23dc27d28600685d2ddfcdc1a22903439651c36
Alpay Aldemir <alpayaldemir@bitgo.com>
2022-12-29 14:44:29 -0500

    rename the Owners NFT contract and symbol

--- contracts/protocol/core/Owners.sol
+++ contracts/protocol/core/Owners.sol
@@ -6,12 +6,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 /**
  * @title Owners ERC721 contract
  * @notice Used as source of truth regarding which account "owns" rights over the funds paid to access a given token
- * on the Content Contract.
+ * on the Content Contract. The owner of a given token id will have the rights to the entirety of funds paid for all access types 
+ * for the respective token id's content. 
+ * A token id on the Content Contract corresponds to the same token id on the Access NFTs and Owners NFT.
  */
 contract Owners is ERC721 {
     address private config;
 
-    constructor(address _contentConfig) ERC721("name", "symbol") {
+    constructor(address _contentConfig) ERC721("Access Payment Owners NFT", "APO") {
         config = _contentConfig;
     }
 }
